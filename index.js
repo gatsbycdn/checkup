26f02ee35266630f5358314e1908e3dfd262b220
tech@gatsbycdn.com <chi_muu@reactjs.lan>
2020-08-17 02:17:58 -0700

    add

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const { apiDAO } = require('./api')
 const Cronjob = require('cron').CronJob
+const express = require('express')
+const app = express()
+const port = 3001
 
 let job = new Cronjob (
     '* */5 * * * *',
@@ -7,3 +10,13 @@ let job = new Cronjob (
     null,
     true
 )
+
+app.get('/test', (req, res) => {
+    apiDAO.updateStatus()
+    res.send('OK')
+})
+
+
+app.listen(port, () => {
+   console.log(`listening at http://localhost:${port}`)
+})
